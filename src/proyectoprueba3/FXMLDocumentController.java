fccc5b8c4060780b877029453bac987fa78e3bc8
SebaBurst <Sebarem723@gmail.com>
2020-03-18 15:31:24 -0300

    Interfaz Grafica

--- src/proyectoprueba3/FXMLDocumentController.java
+++ src/proyectoprueba3/FXMLDocumentController.java
@@ -10,7 +10,11 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -20,8 +24,35 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
-    
     @FXML
+    private TextField FieldNumero;
+    @FXML
+    private TextField FieldCompania;
+    @FXML
+    private MenuButton SeleccionarCompania;
+    @FXML
+    private TextField FieldMensaje;
+    @FXML
+    private Button BotonEnviar;
+    @FXML
+    private Label LabelHora;
+    @FXML
+    private TextField FieldCaptcha;
+    @FXML
+    private Button BotonValidar;
+    @FXML
+    private Button BotonHistorial;
+    @FXML
+    private TextArea AreaTextoRecibido;
+    @FXML
+    private TextField FieldResponder;
+    @FXML
+    private Button BotonResponder;
+    @FXML
+    private TextField FieldFecha;
+    @FXML
+    private TextField FieldHora;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
